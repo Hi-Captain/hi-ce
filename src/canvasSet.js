e9a51565e83ccb79790bc425090b10d96dc73389
hi_captain_ <7273captain@gmail.com>
2018-07-30 21:52:20 +0900

    Add Mix Animation & Finish

--- src/canvasSet.js
+++ src/canvasSet.js
@@ -1,18 +1,30 @@
+//주사위 생성 함수
 const draw = function(canvas, target) {
-  var value = Math.floor(Math.random() * 6) + 1;
+  var cStyle = canvas.style
 
-  switch (value){
-    case 1: draw1(canvas, target); break;
-    case 2: draw2(canvas, target); break;
-    case 3: draw3(canvas, target); break;
-    case 4: draw4(canvas, target); break;
-    case 5: draw5(canvas, target); break;
-    case 6: draw6(canvas, target); break; 
-    default:
-      console.log('not allow')
-  }
+  var mix = setInterval(() => {
+    cStyle.opacity = '0.6'
+    var value = Math.floor(Math.random() * 6) + 1;
+
+    switch (value){
+      case 1: draw1(canvas, target); break;
+      case 2: draw2(canvas, target); break;
+      case 3: draw3(canvas, target); break;
+      case 4: draw4(canvas, target); break;
+      case 5: draw5(canvas, target); break;
+      case 6: draw6(canvas, target); break; 
+      default:
+        console.log('error')
+    }
+  }, 100);
+
+  setTimeout(function(){
+    cStyle.opacity = '1'
+    clearTimeout(mix)
+  }, 1000)
 }
 
+//1번 주사위
 function draw1(canvas, target){
   canvas.width = canvas.width
 
@@ -22,6 +34,7 @@ function draw1(canvas, target){
   target.fill();
 }
 
+//2번 주사위
 function draw2(canvas, target){
   canvas.width = canvas.width
 
@@ -36,6 +49,7 @@ function draw2(canvas, target){
   target.fill();
 }
 
+//3번 주사위
 function draw3(canvas, target){
   canvas.width = canvas.width
 
@@ -55,6 +69,7 @@ function draw3(canvas, target){
   target.fill();
 }
 
+//4번 주사위
 function draw4(canvas, target){
   canvas.width = canvas.width
 
@@ -79,6 +94,7 @@ function draw4(canvas, target){
   target.fill();
 }
 
+//5번 주사위
 function draw5(canvas, target){
   canvas.width = canvas.width
 
@@ -108,6 +124,7 @@ function draw5(canvas, target){
   target.fill();
 }
 
+//6번 주사위
 function draw6(canvas, target){
   canvas.width = canvas.width
 
